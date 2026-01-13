7120790aa1ce30e85e084b53c293d7533d3a7dbd
DenisBorzenkov <denis@lineupds.com>
2026-01-13 17:28:28 +0000

    fix after review++

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
+	"strconv"
 
 	"project_sem/internal/api"
 	"project_sem/internal/db"
 )
 
+const (
+	dbUser     = "validator"
+	dbPassword = "val1dat0r"
+	dbName     = "project-sem-1"
+	dbHost     = "localhost"
+	dbPort     = "5432"
+)
+
 func main() {
+	port, err := strconv.Atoi(dbPort)
+	if err != nil {
+		log.Fatalf("Invalid DB_PORT: %v", err)
+	}
+
 	pg, err := db.NewPostgres(db.PGConfig{
-		Host:     os.Getenv("DB_HOST"),
-		Port:     5432,
-		User:     os.Getenv("DB_USER"),
-		Password: os.Getenv("DB_PASSWORD"),
-		DBName:   os.Getenv("DB_NAME"),
+		Host:     dbHost,
+		Port:     port,
+		User:     dbUser,
+		Password: dbPassword,
+		DBName:   dbName,
 		SSLMode:  "disable",
 	})
 	if err != nil {
